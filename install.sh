d5b9ffc5ac7b415e571227ee95730781e2ce9ed6
Jason Carver <ut96caarrs@snkmail.com>
2014-08-10 14:35:37 -0700

    globally install these python libs

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ DOTFILE_REPO="$( readlink -f $( dirname "$0"))"
 ln -s $DOTFILE_REPO/.screenrc ~/.screenrc
 ln -s $DOTFILE_REPO/.vimrc ~/.vimrc
 ln -s $DOTFILE_REPO/.inputrc ~/.inputrc
+sudo pip install -r $DOTFILE_REPO/requirements.txt
 
