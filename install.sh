5bb57f748a4bd2244977fed7c43a8f56bcf3405a
Jason Carver <ut96caarrs@snkmail.com>
2014-08-19 14:36:54 -0700

    install pip

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ DOTFILE_REPO="$( readlink -f $( dirname "$0"))"
 ln -s $DOTFILE_REPO/.screenrc ~/.screenrc
 ln -s $DOTFILE_REPO/.vimrc ~/.vimrc
 ln -s $DOTFILE_REPO/.inputrc ~/.inputrc
+sudo apt-get update
+sudo apt-get install python-pip
 sudo pip install -r $DOTFILE_REPO/requirements.txt
 
