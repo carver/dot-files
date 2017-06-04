465a6d3b295d9d5fb17af5fca76b35b164617bc7
Jason Carver <ut96caarrs@snkmail.com>
2017-06-03 21:13:31 -0700

    vim is a hard requirement
    curl is required to auto-install the vim plugins

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ ln -s $DOTFILE_REPO/.screenrc ~/.screenrc
 ln -s $DOTFILE_REPO/.vimrc ~/.vimrc
 ln -s $DOTFILE_REPO/.inputrc ~/.inputrc
 sudo apt-get update
-sudo apt-get install -y python-pip
+sudo apt-get install -y python-pip vim curl
 sudo pip install -r $DOTFILE_REPO/requirements.txt
 
