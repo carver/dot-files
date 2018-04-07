fef6899a44eb66968051dc97b15481fc38dd2b59
Jason Carver <ut96caarrs@snkmail.com>
2018-04-07 08:06:40 -0700

    add public key

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ ln -s $DOTFILE_REPO/.vimrc ~/.vimrc
 ln -s $DOTFILE_REPO/.inputrc ~/.inputrc
 mkdir ~/.vim
 ln -s $DOTFILE_REPO/vimconfig/ftplugin ~/.vim/ftplugin
+mkdir ~/.ssh
+cat authorized_keys >>~/.ssh/authorized_keys
 sudo apt-get update
 sudo apt-get install -y python-pip vim curl
 sudo pip install -r $DOTFILE_REPO/requirements.txt
