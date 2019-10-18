9a1769e5b057b4cf446f8f8a95317bb04fdabf6e
Jason Carver <ut96caarrs@snkmail.com>
2019-10-18 05:11:53 -0700

    Install screen and openssh server by default

--- install.sh
+++ install.sh
@@ -14,6 +14,6 @@ ln -s $DOTFILE_REPO/vimconfig/ftplugin ~/.vim/ftplugin
 mkdir ~/.ssh
 cat $DOTFILE_REPO/authorized_keys >>~/.ssh/authorized_keys
 sudo apt-get update
-sudo apt-get install -y python-pip vim curl
+sudo apt-get install -y python-pip vim curl screen openssh-server
 sudo pip install -r $DOTFILE_REPO/requirements.txt
 
