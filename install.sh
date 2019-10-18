923f940349dd423ab54528dee2efb0d4b8e42700
Jason Carver <ut96caarrs@snkmail.com>
2019-10-18 05:11:04 -0700

    Install ssh keys even if not running from repo dir

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -o errexit
+set -o pipefail
+set -o nounset
+
 DOTFILE_REPO="$( readlink -f $( dirname "$0"))"
 
 ln -s $DOTFILE_REPO/.screenrc ~/.screenrc
@@ -8,7 +12,7 @@ ln -s $DOTFILE_REPO/.inputrc ~/.inputrc
 mkdir ~/.vim
 ln -s $DOTFILE_REPO/vimconfig/ftplugin ~/.vim/ftplugin
 mkdir ~/.ssh
-cat authorized_keys >>~/.ssh/authorized_keys
+cat $DOTFILE_REPO/authorized_keys >>~/.ssh/authorized_keys
 sudo apt-get update
 sudo apt-get install -y python-pip vim curl
 sudo pip install -r $DOTFILE_REPO/requirements.txt
