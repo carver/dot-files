58c1ff8e5eeee4d50747d6f7ea2dcba55d818542
Jason Carver <ut96caarrs@snkmail.com>
2020-10-06 21:37:54 -0700

    Newer pip package, that works with venv better

--- install.sh
+++ install.sh
@@ -14,6 +14,6 @@ ln -s $DOTFILE_REPO/vimconfig/ftplugin ~/.vim/ftplugin
 mkdir -p ~/.ssh
 cat $DOTFILE_REPO/authorized_keys >>~/.ssh/authorized_keys
 sudo apt-get update
-sudo apt-get install -y python-pip vim curl screen openssh-server
+sudo apt-get install -y python-pip-whl vim curl screen openssh-server
 sudo pip install -r $DOTFILE_REPO/requirements.txt
 
