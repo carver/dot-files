800389c8e1901219e27a2e3a1d35e1b76ea84b1f
Jason Carver <ut96caarrs@snkmail.com>
2020-10-06 21:32:28 -0700

    Smoothly handle if .ssh/ or .vim/ already exists

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ DOTFILE_REPO="$( readlink -f $( dirname "$0"))"
 ln -s $DOTFILE_REPO/.screenrc ~/.screenrc
 ln -s $DOTFILE_REPO/.vimrc ~/.vimrc
 ln -s $DOTFILE_REPO/.inputrc ~/.inputrc
-mkdir ~/.vim
+mkdir -p ~/.vim
 ln -s $DOTFILE_REPO/vimconfig/ftplugin ~/.vim/ftplugin
-mkdir ~/.ssh
+mkdir -p ~/.ssh
 cat $DOTFILE_REPO/authorized_keys >>~/.ssh/authorized_keys
 sudo apt-get update
 sudo apt-get install -y python-pip vim curl screen openssh-server
